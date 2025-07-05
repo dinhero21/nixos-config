e21f0aa2d507aa546147d393637dd9e02470bbba
dinhero21 <dinhero21@dinhero21.dev>
2025-07-05 09:19:14 -0300

    add discord

--- configuration.nix
+++ configuration.nix
@@ -137,7 +137,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    prismlauncher
+    prismlauncher vesktop
   ];
 
   # Open ports in the firewall.
