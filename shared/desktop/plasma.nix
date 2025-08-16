a2eb16c69431eb45e744d9cdeef90906b490a647
dinhero21 <dinhero21@dinhero21.dev>
2025-08-15 22:07:48 -0300

    configure plasma-manager

--- shared/desktop/plasma.nix
+++ shared/desktop/plasma.nix
@@ -7,4 +7,29 @@
   home-manager.sharedModules = [
     flake-inputs.plasma-manager.homeManagerModules.plasma-manager
   ];
+
+  home-manager.users.dinhero21 = {
+    programs.plasma =
+      let
+        wallpaper = "${pkgs.nixos-artwork.wallpapers.nineish-dark-gray}/share/backgrounds/nixos/nix-wallpaper-nineish-dark-gray.png";
+      in
+    {
+      enable = true;
+
+      workspace = {
+        lookAndFeel = "org.kde.breezedark.desktop";
+        wallpaper = wallpaper;
+      };
+
+      kscreenlocker.appearance.wallpaper = wallpaper;
+
+      powerdevil = {
+        AC = {
+          # this has made me corrupt partitions before, why isn't this the default?
+          # respectfully, fuck you KDE /hj
+          autoSuspend.action = "nothing";
+        };
+      };
+    };
+  };
 }
