04456a255683d79521e4191e85018f3cc22b70a2
dinhero21 <dinhero21@dinhero21.dev>
2026-05-31 05:16:51 -0300

    improve kiosk

--- shared/kiosk.nix
+++ shared/kiosk.nix
@@ -1,30 +1,40 @@
 { config, lib, pkgs, ... }:
 
 let cfg = config.kiosk; in {
-  options = {
-    kiosk = {
-      enable = lib.mkEnableOption "Enable Kiosk - Single Fullscreen App DE";
-      program = lib.mkOption {
-        type = lib.types.str;
-        default = "";
-        description = "program to run, empty string (default) for interactive shell";
-        example = "firefox --kiosk https://dinhero21.dev/";
-      };
-      # TODO:
-      # terminal = lib.mkOption {
-      #   type = lib.types.boolean;
-      #   description = "display program stderr/stdout via foot"
-      # };
+  imports = [
+    ./audio.nix
+  ];
+
+  options.kiosk = {
+    enable = lib.mkEnableOption "Enable Kiosk - Single Fullscreen App DE";
+    program = lib.mkOption {
+      type = lib.types.str;
+      default = "";
+      description = "program to run, empty string (default) for interactive shell";
+      example = "firefox --kiosk https://dinhero21.dev/";
     };
+    # TODO:
+    # terminal = lib.mkOption {
+    #   type = lib.types.boolean;
+    #   default = true;
+    #   description = "display program stderr/stdout via foot";
+    # };
+    # TODO:
+    # drop-capabilities = lib.mkOption {
+    #   type = lib.types.boolean;
+    #   default = true;
+    #   description = "clean ambient capabilities, polluted by services.cage - needed for bwrap";
+    # };
   };
 
   config = lib.mkIf cfg.enable {
     system.nixos.tags = [ "kiosk" ];
-  
+
     fonts.enableDefaultPackages = true;
-  
     programs.foot.enable = true;
-  
+
+    audio.enable = true;
+
     services.cage = {
       enable = true;
       # allow vt-switching
@@ -38,7 +48,11 @@ let cfg = config.kiosk; in {
       };
   
       user = "dinhero21";
-      program = "${pkgs.foot}/bin/foot --maximized --fullscreen ${cfg.program}";
+      # services.cage pollutes ambient capabilities with cap_wake_alarm because of PAM integration
+      # this breaks programs that don't expect it, such as bwrap
+      # to revert it, we use setpriv --ambient-caps -all to clear ambient capabilities
+      program = "${pkgs.foot}/bin/foot --maximized --fullscreen -- ${pkgs.util-linux}/bin/setpriv --ambient-caps -all -- ${cfg.program}";
+      # program = "${pkgs.foot}/bin/foot --maximized --fullscreen -- ${cfg.program}";
       # program = "${pkgs.foot}/bin/foot --maximized --fullscreen";
     };
   };
