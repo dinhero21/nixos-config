23ed64e49691f59bc1f2b939721ffa12023ba982
dinhero21 <dinhero21@dinhero21.dev>
2026-09-18 23:09:51 -0300

    4: static addresses

--- hosts/4/system.nix
+++ hosts/4/system.nix
@@ -7,9 +7,33 @@ flake-inputs.nixpkgs.lib.nixosSystem {
       imports = [
        ./hardware.nix
        ../../shared/common.nix
+       ../../shared/server
       ];
 
+      # TODO: extract into shared/computer-id.nix
+
       networking.hostName = "4";
+
+      networking.firewall = let openPorts = { from = 14000; to = 14999; }; in {
+        allowedTCPPortRanges = [ openPorts ];
+        allowedUDPPortRanges = [ openPorts ];
+      };
+
+      # use dhcp inform instead of static
+      # to autoconfigure gateway and nameservers
+      networking.dhcpcd.enable = true;
+      networking.dhcpcd.extraConfig = ''
+        interface enp43s0
+        inform 192.168.0.104/24
+      '';
+
+      networking.interfaces.enp43s0.ipv6.addresses = [{
+        address = "2804:14d:688c:40a8:7374:6174:6963:4";
+        prefixLength = 64;
+      }];
+
+      # disable other ipv6 addresses
+      boot.kernel.sysctl."net.ipv6.conf.enp43s0.autoconf" = 0;
    })
   ];
 }
